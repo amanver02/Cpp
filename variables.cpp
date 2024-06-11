161d70be3c3b6f2f6c223202a351a854641d173d
amanver02 <155369942+amanver02@users.noreply.github.com>
2024-06-11 23:33:15 +0530

    variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"4";
+  cout<<endl;
+  cout<<4+2; // Works as an operator
+  cout<<endl;
+  cout<<"4+2";// works in text form
+  return 0;
+}
