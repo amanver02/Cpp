c1597bf7b189649970781d98f06fd8067a9e6593
amanver02 <developeraman02@gmail.com>
2024-07-27 16:25:20 +0530

    #

---
+++ CWcpp/sum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using  namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter the first number";
+    cin>>x;
+    cout<<"Enter the second number";
+    cin>>y;
+    cout<<"sum is :"<<x+y;
+}
