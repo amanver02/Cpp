843d682695358d1561d1b9b8535796735f5d7688
amanver02 <developeraman02@gmail.com>
2024-07-27 14:40:42 +0530

    cwcpp

---
+++ CWcpp/user_input_sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    
+    cout<<"Enter the first no :";
+    cin>>x;
+    cout<<"Enter the second no :";
+    cin>>y;
+    int z=x+y;
+    cout<<"The sum of two no are" <<z;
+    return 0;
+}
