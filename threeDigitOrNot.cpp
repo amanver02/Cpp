6e0413f2186640acc031babe082f1a69ac0a839d
amanver02 <developeraman02@gmail.com>
2024-07-31 00:09:30 +0530

    #

---
+++ threeDigitOrNot.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a 3 digit no : ";
+    cin>>n;
+    if(n>99 && n<999){
+        cout<<"Three Digit Number";
+    
+    }
+    else{
+        cout<<"Not a 3 Digit Number";
+    }
+}
