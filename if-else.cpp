9e3bb763c9188ef8de8172fdf25f5ffecf202960
amanver02 <developeraman02@gmail.com>
2024-08-21 00:42:43 +0530

    #

--- if-else.cpp
+++ if-else.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-cout<<"code hai bhai";
+    int age;
+    cout<<"Tell me Your  age"<<endl;
+    cin>>age;
+    if(age<18){
+        cout<<"You can not come to my party"<<endl;
+    }
+
+    else if(age==18){
+        cout<<"You are a kid and you will get a kid pass to the party "<<endl;
+    }
+    else{
+        cout<<"you can come to the party"<<endl;
+    }
+    
 return 0;
 }
